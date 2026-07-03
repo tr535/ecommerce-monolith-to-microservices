8e364c31e042f25181bd5be16882c4e9c7a0ac2a
tr535 <tamar0556780562@gmail.com>
2026-07-03 15:12:02 +0300

    Return order status as string in API responses

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+﻿FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+WORKDIR /src
+
+COPY ["EcommerceMonolith.csproj", "./"]
+RUN dotnet restore "EcommerceMonolith.csproj"
+
+COPY . .
+RUN dotnet publish "EcommerceMonolith.csproj" -c Release -o /app/publish /p:UseAppHost=false
+
+FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS final
+WORKDIR /app
+
+EXPOSE 8080
+
+COPY --from=build /app/publish .
+
+ENTRYPOINT ["dotnet", "EcommerceMonolith.dll"]
--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using EcommerceMonolith.DAL;
 using Microsoft.EntityFrameworkCore;
 using EcommerceMonolith.BLL;
-
+using System.Text.Json.Serialization;
 namespace EcommerceMonolith
 {
     public class Program
@@ -16,14 +16,37 @@ namespace EcommerceMonolith
             builder.Services.AddScoped<IProductService, ProductService>();
             builder.Services.AddScoped<IOrderService, OrderService>();
             // Add services to the container.
-            builder.Services.AddControllers();
-
+            builder.Services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
+                var retryCount = 0;
+                var maxRetries = 10;
+
+                while (retryCount < maxRetries)
+                {
+                    try
+                    {
+                        dbContext.Database.Migrate();
+                        break;
+                    }
+                    catch
+                    {
+                        retryCount++;
+                        Thread.Sleep(5000);
+                    }
+                }
+            }
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
